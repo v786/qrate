d122e2a29c4bf8d7ba3d68a731772e44b48a0c82
v786 <jahangirali786@gmail.com>
2021-11-08 08:09:24 +0530

    Added specific folder for storage option

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
         return self.name
 
 class Resume(models.Model):
-    file = models.FileField()
+    file = models.FileField(upload_to='storage/')
     name = models.CharField(max_length=30)
     created_at = models.DateField(auto_now=True)
     profile = models.ForeignKey(Profile, related_name='resume', on_delete=models.CASCADE)
